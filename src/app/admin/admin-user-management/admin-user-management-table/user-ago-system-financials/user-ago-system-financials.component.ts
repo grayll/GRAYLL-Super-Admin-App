4a83a16daa4df0b724347a3ebee3f03d6cdc6d78
nedim1511 <nedim.kurbegovic@stu.ssst.edu.ba>
2019-10-22 13:46:20 +0200

    implement user algo system financials

--- src/app/admin/admin-user-management/admin-user-management-table/user-ago-system-financials/user-ago-system-financials.component.ts
+++ src/app/admin/admin-user-management/admin-user-management-table/user-ago-system-financials/user-ago-system-financials.component.ts
@@ -1,15 +1,200 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, SimpleChanges} from '@angular/core';
+import {
+	faArrowAltCircleDown,
+	faCaretDown, faCaretUp,
+	faCopy,
+	faInfoCircle,
+	faSearch,
+	faTimesCircle
+} from '@fortawesome/free-solid-svg-icons';
+import {ClipboardService} from 'ngx-clipboard';
+import {SnotifyService} from 'ng-snotify';
 
 @Component({
   selector: 'app-user-ago-system-financials',
   templateUrl: './user-ago-system-financials.component.html',
-  styleUrls: ['./user-ago-system-financials.component.css']
+  styleUrls: ['./user-ago-system-financials.component.scss']
 })
 export class UserAgoSystemFinancialsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+	
+	selectedTab: {id: string, name: string};
+	isSortedUpBySignUpDate: boolean;
+	isSortedUpByCurrentAlgoPositionsUSD: boolean;
+	isSortedUpByHistoricalAlgoPositionsUSD: boolean;
+	isSortedUpByAlgoCommissionsUSD: boolean;
+	isSortedUpByOpenAlgoPositions: boolean;
+	isSortedUpByClosedAlgoPositions: boolean;
+	isSortedUpByGRYOpenPositions: boolean;
+	isSortedUpByGRY1OpenPositions: boolean;
+	isSortedUpByGRY2OpenPositions: boolean;
+	isSortedUpByGRY3OpenPositions: boolean;
+	isSortedUpByGRZOpenPositions: boolean;
+	userAccountTabs = [
+		{
+			id: 'uapg',
+			name: 'User Algo Positions General'
+		},
+		{
+			id: 'uapd',
+			name: 'User Algo Positions Detail'
+		}
+	];
+	
+	// Font Awesome Icons
+	faDownload = faArrowAltCircleDown;
+	faClose = faTimesCircle;
+	faInfo = faInfoCircle;
+	faCopy = faCopy;
+	faSearch = faSearch;
+	faSortBySignUpDate = faCaretDown;
+	faSortByCurrentAlgoPositionsUSD = faCaretDown;
+	faSortByHistoricalAlgoPositionsUSD = faCaretDown;
+	faSortByAlgoCommissionsUSD = faCaretDown;
+	faSortByOpenAlgoPositions = faCaretDown;
+	faSortByClosedAlgoPositions = faCaretDown;
+	faSortByGRYOpenPositions = faCaretDown;
+	faSortByGRY1OpenPositions = faCaretDown;
+	faSortByGRY2OpenPositions = faCaretDown;
+	faSortByGRY3OpenPositions = faCaretDown;
+	faSortByGRZOpenPositions = faCaretDown;
+	
+	constructor(
+	private clipboardService: ClipboardService,
+	private snotifyService: SnotifyService
+	) {
+		this.setActiveTab();
+	}
+	
+	ngOnInit() {}
+	
+	ngOnChanges(changes: SimpleChanges): void {
+		if (changes.activeTabId && changes.activeTabId.currentValue) {
+			this.selectedTab = this.userAccountTabs.find((t) => t.id === changes.activeTabId.currentValue);
+		}
+	}
+	
+	private setActiveTab() {
+		this.selectedTab = this.userAccountTabs[0];
+	}
+	
+	sortBySignUpDate() {
+		if (this.isSortedUpBySignUpDate) {
+			this.faSortBySignUpDate = faCaretDown;
+			this.isSortedUpBySignUpDate = false;
+		} else {
+			this.faSortBySignUpDate = faCaretUp;
+			this.isSortedUpBySignUpDate = true;
+		}
+	}
+	
+	sortByCurrentAlgoPositionUSD() {
+		if (this.isSortedUpByCurrentAlgoPositionsUSD) {
+			this.faSortByCurrentAlgoPositionsUSD = faCaretDown;
+			this.isSortedUpByCurrentAlgoPositionsUSD = false;
+		} else {
+			this.faSortByCurrentAlgoPositionsUSD = faCaretUp;
+			this.isSortedUpByCurrentAlgoPositionsUSD = true;
+		}
+	}
+	
+	sortByHistoricalAlgoPositionUSD() {
+		if (this.isSortedUpByHistoricalAlgoPositionsUSD) {
+			this.faSortByHistoricalAlgoPositionsUSD = faCaretDown;
+			this.isSortedUpByHistoricalAlgoPositionsUSD = false;
+		} else {
+			this.faSortByHistoricalAlgoPositionsUSD = faCaretUp;
+			this.isSortedUpByHistoricalAlgoPositionsUSD = true;
+		}
+	}
+	
+	sortByAlgoCommissionsUSD() {
+		if (this.isSortedUpByAlgoCommissionsUSD) {
+			this.faSortByAlgoCommissionsUSD = faCaretDown;
+			this.isSortedUpByAlgoCommissionsUSD = false;
+		} else {
+			this.faSortByAlgoCommissionsUSD = faCaretUp;
+			this.isSortedUpByAlgoCommissionsUSD = true;
+		}
+	}
+	
+	sortByOpenAlgoPositions() {
+		if (this.isSortedUpByOpenAlgoPositions) {
+			this.faSortByOpenAlgoPositions = faCaretDown;
+			this.isSortedUpByOpenAlgoPositions = false;
+		} else {
+			this.faSortByOpenAlgoPositions = faCaretUp;
+			this.isSortedUpByOpenAlgoPositions = true;
+		}
+	}
+	
+	sortByClosedAlgoPositions() {
+		if (this.isSortedUpByClosedAlgoPositions) {
+			this.faSortByClosedAlgoPositions = faCaretDown;
+			this.isSortedUpByClosedAlgoPositions = false;
+		} else {
+			this.faSortByClosedAlgoPositions = faCaretUp;
+			this.isSortedUpByClosedAlgoPositions = true;
+		}
+	}
+	
+	sortByGRYOpenPositions() {
+		if (this.isSortedUpByGRYOpenPositions) {
+			this.faSortByGRYOpenPositions = faCaretDown;
+			this.isSortedUpByGRYOpenPositions = false;
+		} else {
+			this.faSortByGRYOpenPositions = faCaretUp;
+			this.isSortedUpByGRYOpenPositions = true;
+		}
+	}
+	
+	sortByGRY1OpenPositions() {
+		if (this.isSortedUpByGRY1OpenPositions) {
+			this.faSortByGRY1OpenPositions = faCaretDown;
+			this.isSortedUpByGRY1OpenPositions = false;
+		} else {
+			this.faSortByGRY1OpenPositions = faCaretUp;
+			this.isSortedUpByGRY1OpenPositions = true;
+		}
+	}
+	
+	sortByGRY2OpenPositions() {
+		if (this.isSortedUpByGRY2OpenPositions) {
+			this.faSortByGRY2OpenPositions = faCaretDown;
+			this.isSortedUpByGRY2OpenPositions = false;
+		} else {
+			this.faSortByGRY2OpenPositions = faCaretUp;
+			this.isSortedUpByGRY2OpenPositions = true;
+		}
+	}
+	
+	sortByGRY3OpenPositions() {
+		if (this.isSortedUpByGRY3OpenPositions) {
+			this.faSortByGRY3OpenPositions = faCaretDown;
+			this.isSortedUpByGRY3OpenPositions = false;
+		} else {
+			this.faSortByGRY3OpenPositions = faCaretUp;
+			this.isSortedUpByGRY3OpenPositions = true;
+		}
+	}
+	
+	sortByGRZOpenPositions() {
+		if (this.isSortedUpByGRZOpenPositions) {
+			this.faSortByGRZOpenPositions = faCaretDown;
+			this.isSortedUpByGRZOpenPositions = false;
+		} else {
+			this.faSortByGRZOpenPositions = faCaretUp;
+			this.isSortedUpByGRZOpenPositions = true;
+		}
+	}
+	
+	onTabChange(id: string) {
+		this.selectedTab = this.userAccountTabs.find((t) => t.id === id);
+	}
+	
+	copySuccess(account: string) {
+		if (this.clipboardService.copyFromContent(account)) {
+			this.snotifyService.simple('Copied to clipboard.');
+		}
+	}
 
 }
